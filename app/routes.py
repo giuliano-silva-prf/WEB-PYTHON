a3a8e59919f26929ddd40bf8cd20dbd57e48f0af
Giuliano Silva <giuliano.silva@prf.gov.br>
2023-12-17 21:51:53 -0300

    Seta a página do curriculo tanto para o / quanto para o /curriculo

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 @app.route('/')
 def home():
-    return 'Hello world!'
+    return redirect(url_for('fcurriculo'))
 @app.route('/curriculo')
-def curriculo():
+def fcurriculo():
     return render_template('index.html')
