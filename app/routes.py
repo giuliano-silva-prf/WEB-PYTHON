d3b5cdfe7794c4801c9e660402e7f9d89167e5ae
Giuliano Silva <giuliano.silva@prf.gov.br>
2023-12-19 12:19:24 -0300

    Implantação do template bootstrap4 SBadmin

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,38 @@ from app import app
 from flask import render_template, redirect, url_for
 
 @app.route('/')
-def home():
-    return redirect(url_for('fcurriculo'))
-@app.route('/curriculo')
-def fcurriculo():
+@app.route('/index')
+def index():
     return render_template('index.html')
+
+@app.route('/layout_static')
+def layout_static():
+    return render_template('layout_static')
+
+@app.route('/layout_sidenav_light')
+def layout_sidenav_light():
+    return render_template('layout_sidenav_light')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/password')
+def password():
+    return render_template('password.html')
+@app.route('/page_401')
+def page_401():
+    return render_template('page_401.html')
+@app.route('/page_404')
+def page_404():
+    return render_template('page_404.html')
+@app.route('/page_500')
+def page_500():
+    return render_template('page_500.html')
+@app.route('/charts')
+def charts():
+    return render_template('charts.html')
+@app.route('/tables')
+def tables():
+    return render_template('tables.html')
--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,34 +1,26 @@
 /*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
+    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2023 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    // 
 // Scripts
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+    }
 
 });
